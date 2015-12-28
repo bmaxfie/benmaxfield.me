2c6480cfcfe68b6b0665f1a6541b81687b03846e
Ben Maxfield <bmaxfie@purdue.edu>
2015-12-28 14:57:39 -0500

    FINALLY got my site to deploy without 5xx errors. Used ebextensions commands to install lxml dependencies and eventually lxml (which is an extremely fickle package). Also readded lxml to requirements.txt. However, css and js are not showing up on deploy.

--- py_workspace/Boilermake15/scripts/01prepenvironment.sh
+++
@@ -1,3 +0,0 @@
-sudo yum install libxml2-devel libxslt-devel python-devel gcc
-easy_install pip
-sudo pip install lxml
---
+++ py_workspace/Boilermake15/scripts/03_prepenvironment.sh
@@ -0,0 +1,3 @@
+sudo yum install libxml2-devel libxslt-devel python-devel python-lxml gcc
+sudo easy_install pip
+sudo pip install lxml
